bf88731a625d33e32bbf084cc397e99d253ab8fa
future1012 <tengyue1023@163.com>
2019-12-06 13:47:27 +0800

    super

--- heima/煎饼果子.py
+++ heima/煎饼果子.py
@@ -1,64 +1,29 @@
-class Master(object):
-    def __init__(self):
-        self.kongfu = '[古法煎饼果子配方]'
+class Dog(object):
+    def work(self):
+        print('指哪儿打哪儿')
 
-    def make_cake(self):
-        print(f'使用{self.kongfu}制作煎饼果子')
+class ArmyDog(Dog):
+    def work(self):
+        print('追击敌人')
 
-class School(object):
-    def __init__(self):
-        self.kongfu = '[黑马煎饼果子配方]'
+class DrugDog(Dog):
+    def work(self):
+        print('检测毒品')
 
-    def make_cake(self):
-        print(f'使用{self.kongfu}制作煎饼果子')
+class Persion(object):
+    def work_with_dog(self,dog):
+        return dog.work()
 
+class Sdog(Dog):
+    def superTest(self):
+        super().work()
 
-class Prentice(Master,School):
-    def __init__(self):
-        #super().__init__()
-        self.kongfu = '[独创煎饼果子配方]'
-        self.__money = 2000000
-
-    def __info_print(self):
-        print('这是私有方法')
-
-    #获取私有属性
-    def get_money(self):
-        return self.__money
-
-    #修改私有属性
-    def set_money(self,value):
-        self.__money = value
-
-    def make_cake(self):
-        self.__init__()
-        print(f'使用{self.kongfu}制作煎饼果子')
-
-    def make_master_cake(self):
-        Master.__init__(self)
-        Master.make_cake(self)
-
-    def make_school_cake(self):
-        School.__init__(self)
-        School.make_cake(self)
+daqiu = Persion()
+A = ArmyDog()
+B = DrugDog()
+daqiu.work_with_dog(A)
+daqiu.work_with_dog(B)
 
 
-
-
-daqiu = Prentice()
-
-
-daqiu.make_cake()
-daqiu.make_master_cake()
-daqiu.make_school_cake()
-daqiu.make_cake()
-# daqiu._Prentice__money = 33333333333
-# print(daqiu._Prentice__money)
-# daqiu._Prentice__info_print()
-
-money = daqiu.get_money()
-print(money)
-
-money = daqiu.set_money(666666)
-money = daqiu.get_money()
-print(money)
+s = Sdog()
+s.superTest()
